b47dd18e339fe14cdd5c46ebd8196235cdd32b43
rodgersxy <rodgersny99@gmail.com>
2023-11-04 00:51:38 +0300

    todo app

---
+++ Todo-App/todo/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Task
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(TaskDelete,self).form_valid(form)
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title','description','completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TaskUpdate,self).form_valid(form)
+    
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title','description','completed']
+    success_url = reverse_lazy('tasks')
+    
+   
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The task was created successfully.")
+        return super(TaskCreate,self).form_valid(form)
+        
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    context_object_name = 'task'
+    
+
+class TaskList(LoginRequiredMixin,ListView):
+    model = Task
+    context_object_name = 'tasks'
+    
+    
+def home(request):
+    return render(request,'home.html')
+   
