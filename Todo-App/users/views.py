9128213b4431f80449c20280cf25e77041fafe43
rodgersxy <rodgersny99@gmail.com>
2023-11-05 10:46:48 +0300

    update the todo app

--- Todo-App/users/views.py
+++ Todo-App/users/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login 
+from django.contrib.auth.models import User
+from .form import RegisterForm
 
-
-
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+            
+        return super(RegisterView, self).form_valid(form)
+    
+    
 class MyLoginView(LoginView):
     template_name = 'users/login.html'
-    fields = '__all__'
     redirect_authenticated_user = True
     
     def get_success_url(self):
