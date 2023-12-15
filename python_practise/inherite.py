7fb5bd5b21bda65be5828e214a4cc1cfc885cbb7
rodgersxy <rodgersny99@gmail.com>
2023-12-15 23:52:57 +0300

    update

---
+++ python_practise/inherite.py
@@ -0,0 +1,14 @@
+# write python code showing inheritance
+
+# 
+class Dog:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
---
+++ python_practise/smallest_num.py
@@ -0,0 +1,14 @@
+# find the smallest number in the list
+# start by writting pseudocode
+
+# def find_smallest_number(numbers):
+#     smallest_number = numbers[0]
+#     for number in numbers:
+#         if number < smallest_number:
+#             smallest_number = number
+#     return smallest_number
+
+
+# simple solution
+def findSmallestInt(arr):
+    return min(arr)
