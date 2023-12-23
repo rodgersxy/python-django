dcdfb821527c9a34c9ad35432b35e5435adb0cf8
rodgersxy <rodgersny99@gmail.com>
2023-12-23 11:09:03 +0300

    update

---
+++ codewars/friends.py
@@ -0,0 +1,6 @@
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+# Otherwise, you can be sure he's not...
+
+def friend(x):
+    return [name for name in x if len(name) == 4]
